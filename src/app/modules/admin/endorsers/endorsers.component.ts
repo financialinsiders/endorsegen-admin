f08467edf9c26d10cb2bc3294a2d0e6005d8f79a
Neil <neil@financialinsiders.ca>
2024-03-22 17:44:13 -0700

    Updated Endorser HTML to include sort and search.

--- src/app/modules/admin/endorsers/endorsers.component.ts
+++ src/app/modules/admin/endorsers/endorsers.component.ts
@@ -1,16 +1,27 @@
 import { CommonModule } from '@angular/common';
+import { TextFieldModule } from '@angular/cdk/text-field';
 import { Component, ViewEncapsulation } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatRippleModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTabsModule } from '@angular/material/tabs';
 import { FirestoreService } from '../services/firestore.service';
+import { TranslocoModule } from '@ngneat/transloco';
+import { FormControl, FormsModule, ReactiveFormsModule, UntypedFormBuilder, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
     selector: 'endorsers',
     standalone: true,
     templateUrl: './endorsers.component.html',
-    imports: [CommonModule],
+    imports: [TranslocoModule, CommonModule,MatIconModule, MatButtonModule, MatRippleModule, MatMenuModule, MatTabsModule, MatButtonToggleModule, FormsModule, MatFormFieldModule, TextFieldModule, ReactiveFormsModule ],
     encapsulation: ViewEncapsulation.None,
 })
 export class EndorsersComponent {
     endorsers: any[] = [];
+    selectedProject: string = 'Sort By:';
     constructor(private firestoreService: FirestoreService) {}
     ngOnInit(): void {
         const agentId = '493';
